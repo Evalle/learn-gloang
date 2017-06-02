8137e8e453d19142c0f4cdb15a92353272db306f
evalle <shmarnev@gmail.com>
2017-06-02 06:27:09 +0200

    add stringutil package

---
+++ src/udemy/03_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "github.com/Evalle/learn-golang/tree/master/src/udemy/03_package/stringutil"
+)
+
+func main()  {
+  fmt.Println(stringutil.Reverse("!oG, olleH"))
+  fmt.Println(stringutil.Myname)
+}
