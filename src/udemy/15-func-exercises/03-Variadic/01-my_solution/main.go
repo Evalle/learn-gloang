a6513c778fd7842ea28ca82f8ac41d5d626adbf4
evalle <shmarnev@gmail.com>
2017-06-19 06:16:54 +0200

    Add solution for the variadic exercise

---
+++ src/udemy/15-func-exercises/03-Variadic/01-my_solution/main.go
@@ -0,0 +1,28 @@
+// Write a function with one variadic parameter that finds the greatest number
+// in a list of numbers.
+//
+// [_Variadic functions_](http://en.wikipedia.org/wiki/Variadic_function)
+// can be called with any number of trailing arguments.
+// For example, `fmt.Println` is a common variadic
+// function.
+package main
+
+import "fmt"
+
+func findBiggest(sf ...int) int {
+	fmt.Println(sf)
+	fmt.Printf("%T \n", sf)
+	var biggest int
+	for _, v := range sf {
+		fmt.Println(v)
+		if v > biggest {
+			biggest = v
+		}
+	}
+	return biggest
+}
+
+func main() {
+	n := findBiggest(2, 4, 6, 8)
+	fmt.Println(n)
+}
