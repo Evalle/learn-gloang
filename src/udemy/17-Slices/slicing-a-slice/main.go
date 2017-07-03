80e8142f695263d350b01aa01bdcb991afbb8604
evalle <shmarnev@gmail.com>
2017-07-03 05:58:37 +0200

    Fix a typo

--- src/udemy/17-Slices/slicing-a-slice/main.go
+++ src/udemy/17-Slices/slicing-a-slice/main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	mySlice := []int{1, 3, 5, 7, 9, 11}
 	fmt.Println(mySlice)
-	fmt.Println("[1:2] - ", mySlice[1:3])
+	fmt.Println("[1:3] - ", mySlice[1:3])
 	fmt.Println("[4:] - ", mySlice[4:])
 }
