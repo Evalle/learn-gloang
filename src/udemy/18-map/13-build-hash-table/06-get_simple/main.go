b2dfb779e0fc531b8f9fc7dc21326f2c7477efc1
evalle <shmarnev@gmail.com>
2017-07-06 16:11:55 +0200

    Add simple example of get

---
+++ src/udemy/18-map/13-build-hash-table/06-get_simple/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	response, _ := http.Get("http://www.gutenberg.org/files/2701/old/moby10b.txt")
+	page, _ := ioutil.ReadAll(response.Body)
+	fmt.Printf("%s", page)
+}
