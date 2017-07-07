2a24d4791c5f84a9e82da2fec257cb9c92ef496b
evalle <shmarnev@gmail.com>
2017-07-07 08:04:31 +0200

    Add scanner

---
+++ src/udemy/18-map/13-build-hash-table/08-scanner/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// an artificial input
+	const input = " Here is to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They are not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can not do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do."
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	// Set the split function for the scanner operation
+	scanner.Split(bufio.ScanWords)
+	// Count the words.
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+}
